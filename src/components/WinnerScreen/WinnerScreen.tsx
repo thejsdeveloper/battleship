cd9b1f0c357ca40ee67883b025c02158e00c0afd
vikas yadav <thejsdeveloper@gmail.com>
2022-04-17 21:13:33 +0800

    removing hardcoded winner name

--- src/components/WinnerScreen/WinnerScreen.tsx
+++ src/components/WinnerScreen/WinnerScreen.tsx
@@ -2,13 +2,13 @@ import { Player } from "../../state/types";
 
 type WinnerScreenProps = {
   onReset: () => void;
-  winner?: Player;
+  winner: Player;
 };
 export const WinnerScreen = ({ onReset, winner }: WinnerScreenProps) => {
   return (
     <div className="main">
       <div className="splash-title font-gradient">Battleship</div>
-      <h1>Winner is Vikas 🎉 </h1>
+      <h1>Winner is {winner.name} 🎉 </h1>
       <button className="button-grad btn-splash btn-icon" onClick={onReset}>
         PLAY AGAIN ?
       </button>
